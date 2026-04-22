39a85869cff865dd3e3a1a3426b735ff7831158c
lexasss <oleg.spakov@tuni.fi>
2026-04-22 12:48:45 +0300

    enabling MVVM in MainWindow

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -9,47 +9,4 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-
-    private void LoadGazingTimeline_Click(object sender, RoutedEventArgs e)
-    {
-        OpenFileDialog ofd = new OpenFileDialog()
-        {
-            Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
-        };
-
-        if (ofd.ShowDialog() == true)
-        {
-            var records = Services.TimelineDataParser.Parse(ofd.FileName);
-            if (records != null)
-            {
-                var renderer = new Services.TimelineRenderer();
-                //TimelineImage.Source = renderer.Render(records);
-                Services.TimelineRenderer.VisualHost? host = renderer.GetVisualHost(records);
-                scvTimeline.Content = host;
-
-                Title = $"Creanex Data Visualization - {System.IO.Path.GetFileName(ofd.FileName)}";
-            }
-        }
-    }
-
-    private void LoadGazePoint_Click(object sender, RoutedEventArgs e)
-    {
-        OpenFileDialog ofd = new OpenFileDialog()
-        {
-            Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
-        };
-
-        if (ofd.ShowDialog() == true)
-        {
-            var records = Services.VarjoDataParser.Parse(ofd.FileName);
-            if (records != null)
-            {
-                var renderer = new Services.GazePlotRenderer();
-                Services.GazePlotRenderer.VisualHost? host = renderer.GetVisualHost(records);
-                scvGazePoints.Content = host;
-
-                Title = $"Creanex Data Visualization - {System.IO.Path.GetFileName(ofd.FileName)}";
-            }
-        }
-    }
 }
---
+++ src/ViewModels/MainViewVM.cs
@@ -0,0 +1,61 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+
+namespace CreanexDataVis.ViewModels;
+
+internal partial class MainViewVM : ObservableObject
+{
+    [ObservableProperty]
+    public partial string Title { get; set; } = MainTitle;
+
+    [ObservableProperty]
+    public partial Services.TimelineRenderer.VisualHost? Timeline { get; set; }
+
+    [ObservableProperty]
+    public partial Services.GazePlotRenderer.VisualHost? GazePlot { get; set; }
+
+    // Internal
+
+    readonly static string MainTitle = "Creanex Data Visualization";
+
+    [RelayCommand]
+    private void LoadCreanexData()
+    {
+        var ofd = new OpenFileDialog()
+        {
+            Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+        };
+
+        if (ofd.ShowDialog() == true)
+        {
+            var records = Services.TimelineDataParser.Parse(ofd.FileName);
+            if (records != null)
+            {
+                var renderer = new Services.TimelineRenderer();
+                Timeline = renderer.GetVisualHost(records);
+                Title = $"{MainTitle} - {System.IO.Path.GetFileName(ofd.FileName)}";
+            }
+        }
+    }
+
+    [RelayCommand]
+    private void LoadVarjoData()
+    {
+        OpenFileDialog ofd = new OpenFileDialog()
+        {
+            Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+        };
+
+        if (ofd.ShowDialog() == true)
+        {
+            var records = Services.VarjoDataParser.Parse(ofd.FileName);
+            if (records != null)
+            {
+                var renderer = new Services.GazePlotRenderer();
+                GazePlot = renderer.GetVisualHost(records);
+                Title = $"{MainTitle} - {System.IO.Path.GetFileName(ofd.FileName)}";
+            }
+        }
+    }
+}
